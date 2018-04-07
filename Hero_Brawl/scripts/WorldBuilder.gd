8d8ef49df3b58b3ebc0337864670e9b12c5b7a52
tombarwell <tomb@rwell.co.uk>
2018-04-07 16:20:29 +0100

    complete meadow background and wall generator. Player positioning for one player

--- Hero_Brawl/scripts/WorldBuilder.gd
+++ Hero_Brawl/scripts/WorldBuilder.gd
@@ -16,12 +16,35 @@ func _ready():
 #	pass
 
 func setup_board():
-	place_walls(200)
-	#set_process(true)
-	#set_process_input(true)
+	place_ext_walls(480, 24)
+	place_players(1)
+	place_walls(20)
 	
+
+func place_players(numPlayers):
+	place_player(560, 560)
+
+func place_player(x, y):
+	var playerScene = load("res://_scenes/Player.tscn")
+	var player = playerScene.instance()
+	player.translate(Vector2(x,y))
+	get_node(".").call_deferred("add_child", player)
+
+func place_ext_walls(boardSize, wallSize):
+	for x in range(boardSize/wallSize):
+		# walls along top and bottom
+		place_wall((x*(wallSize))+132, 108)
+		place_wall((x*(wallSize))+132, 612)
+		# walls along left and right
+		place_wall(108, (x*(wallSize))+132)
+		place_wall(612, (x*(wallSize))+132)
+		
+	
+
+#func place_length_wall(x, y, length, horv):
+
 func place_wall(x, y):
-	var wallScene = load("res://Wall.tscn")
+	var wallScene = load("res://_scenes/Wall.tscn")
 	var wall = wallScene.instance()
 	wall.translate(Vector2(x,y))
 	get_node(".").call_deferred("add_child", wall)
