bc394779c2547fa158ab7acb68dc0cc6f412cdb0
cca3 <cca3@st-andrews.ac.uk>
2018-04-07 19:20:42 +0100

    WHY PLS WHY

--- Hero_Brawl/scripts/PlayerKinematicBody.gd
+++ Hero_Brawl/scripts/PlayerKinematicBody.gd
@@ -1,9 +1,12 @@
 extends RigidBody2D
 
 # Members
-var velocity = Vector2()
-
 export var PlayerAccel = 100
+export var PlayerAccelDrag = 0.5
+export var PlayerTurn = 0.1
+export var PlayerTurnDrag = 0.1
+
+export var health = 100
 
 onready var sprite = get_node("PlayerSprite")
 
@@ -13,17 +16,48 @@ func _ready():
 	set_process(true)
 	
 func _process(delta):
-
+	
+	motion(delta)
+	if Input.is_action_just_pressed("Attack"):
+		attack()
 		
+	
+func motion(delta):	
+	apply_impulse(Vector2(0, 0), keyboardMotion(delta))
+	
+func joyMotion(delta):
+	var force = Vector2(0,0)
+	
+	if Input.get_joy_axis(0, 7) > 0:
+		force = Vector2(cos(rotation), sin(rotation)) * PlayerAccel
+		
+	var turn = 0
+	if Input.get_joy_axis(0, 0) != 0:
+		set_angular_velocity(get_angular_velocity() + Input.get_joy_axis(0, 0) * PlayerTurn)
+	else:
+		set_angular_velocity(get_angular_velocity() * PlayerTurnDrag)
+	
+	var motion = force * delta
+	
+	return motion
+	
+func keyboardMotion(delta):
+	var force = Vector2(0,0)
+	
 	if Input.is_key_pressed(KEY_RIGHT):
-		velocity = Vector2(PlayerAccel, 0)
+		force = Vector2(PlayerAccel, 0)
 	elif Input.is_key_pressed(KEY_LEFT):
-		velocity = Vector2(-PlayerAccel, 0)
+		force = Vector2(-PlayerAccel, 0)
 	elif Input.is_key_pressed(KEY_UP):
-		velocity = Vector2(0, -PlayerAccel)
+		force = Vector2(0, -PlayerAccel)
 	elif Input.is_key_pressed(KEY_DOWN):
-		velocity = Vector2(0, PlayerAccel)
+		force = Vector2(0, PlayerAccel)
+	else:
+		force = Vector2(0, 0)
+	var motion = force * delta
 	
+	return motion
 	
-	var motion = velocity * delta
-	add_force(Vector2(0, 0), motion)
+func attack():
+	pass
+	
